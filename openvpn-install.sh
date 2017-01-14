f3d8ae54c4a6b8e42854f3df6ebfdb4e220be53d
D. Robin <dyrda.robin@outlook.fr>
2017-01-14 04:41:27 +0100

    Missing "-get" for OpenVPN install on Debian 8.x

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -317,7 +317,7 @@ else
 		if [[ "$VERSION_ID" = 'VERSION_ID="8"' ]]; then
 			echo "deb http://swupdate.openvpn.net/apt jessie main" > /etc/apt/sources.list.d/swupdate-openvpn.list
 			wget -O - https://swupdate.openvpn.net/repos/repo-public.gpg | apt-key add -
-			apt update
+			apt-get update
 		fi
 		# Ubuntu 12.04
 		if [[ "$VERSION_ID" = 'VERSION_ID="12.04"' ]]; then
