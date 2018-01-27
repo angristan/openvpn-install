cffe4bee4ae9c8b27462255e4cbb026f9a4bcc0c
Angristan <angristan@users.noreply.github.com>
2018-01-27 20:21:28 +0100

    Inverse FDN's DNS servers for DNSSEC
    The .12 does not validate DNSSEC while the .40 does, so I'm putting the .40 first.

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -516,8 +516,8 @@ ifconfig-pool-persist ipp.txt" >> /etc/openvpn/server.conf
 		echo 'push "dhcp-option DNS 9.9.9.9"' >> /etc/openvpn/server.conf
 		;;
 		3) #FDN
-		echo 'push "dhcp-option DNS 80.67.169.12"' >> /etc/openvpn/server.conf
 		echo 'push "dhcp-option DNS 80.67.169.40"' >> /etc/openvpn/server.conf
+		echo 'push "dhcp-option DNS 80.67.169.12"' >> /etc/openvpn/server.conf
 		;;
 		4) #DNS.WATCH
 		echo 'push "dhcp-option DNS 84.200.69.80"' >> /etc/openvpn/server.conf
