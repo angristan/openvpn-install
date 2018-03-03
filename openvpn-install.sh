a4e1e0cfc2cf89591be4094bfc1f96533c76907b
Angristan <angristan@users.noreply.github.com>
2018-03-04 00:49:33 +0100

    Remove useless apt update

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -517,7 +517,6 @@ else
 		elif [[ "$VERSION_ID" = 'VERSION_ID="9"' ]]; then
 			echo "deb http://build.openvpn.net/debian/openvpn/stable stretch main" > /etc/apt/sources.list.d/openvpn.list
 			wget -O - https://swupdate.openvpn.net/repos/repo-public.gpg | apt-key add -
-			apt update
 		fi
 		# Ubuntu 14.04
 		elif [[ "$VERSION_ID" = 'VERSION_ID="14.04"' ]]; then
