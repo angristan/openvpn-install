c14355a34c3670de73021b90ce28a4f3271d9c26
Jun Hui <fjh828@gmail.com>
2018-09-23 23:09:33 +0800

    Update easy-rsa to 3.0.5 (#301)

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -586,13 +586,13 @@ function installOpenVPN () {
 	fi
 
 	# Install the latest version of easy-rsa from source
-	local version="3.0.4"
-	wget -O ~/EasyRSA-${version}.tgz https://github.com/OpenVPN/easy-rsa/releases/download/v${version}/EasyRSA-${version}.tgz
-	tar xzf ~/EasyRSA-${version}.tgz -C ~/
+	local version="3.0.5"
+	wget -O ~/EasyRSA-nix-${version}.tgz https://github.com/OpenVPN/easy-rsa/releases/download/v${version}/EasyRSA-nix-${version}.tgz
+	tar xzf ~/EasyRSA-nix-${version}.tgz -C ~/
 	mv ~/EasyRSA-${version}/ /etc/openvpn/
 	mv /etc/openvpn/EasyRSA-${version}/ /etc/openvpn/easy-rsa/
 	chown -R root:root /etc/openvpn/easy-rsa/
-	rm -f ~/EasyRSA-${version}.tgz
+	rm -f ~/EasyRSA-nix-${version}.tgz
 
 	cd /etc/openvpn/easy-rsa/
 	case $CERT_TYPE in
