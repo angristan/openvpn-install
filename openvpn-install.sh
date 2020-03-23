671e21de3ce8b6e49bb424385ec83280bc16613b
Swamy Goundar <psg@southecho.com>
2020-03-22 18:54:53 -0700

    Added PiVPN Function listcerts

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -1230,6 +1230,39 @@ function removeOpenVPN () {
 	fi
 }
 
+function listcerts () {
+# PiVPN: list clients script function
+
+INDEX="/etc/openvpn/easy-rsa/pki/index.txt"
+printf "\n"
+if [ ! -f "${INDEX}" ]; then
+        echo "The file: $INDEX was not found!"
+        exit 1
+fi
+
+printf ": NOTE : The first entry should always be your valid server!\n"
+printf "\n"
+printf "\e[1m::: Certificate Status List :::\e[0m\n"
+printf " ::\e[4m  Status  \e[0m||\e[4m   Name   \e[0m:: \n"
+
+while read -r line || [ -n "$line" ]; do
+    STATUS=$(echo "$line" | awk '{print $1}')
+    NAME=$(echo "$line" | sed -e 's:.*/CN=::')
+    if [ "${STATUS}" == "V" ]; then
+        printf "     Valid   ::   %s\n" "$NAME"
+    elif [ "${STATUS}" == "R" ]; then
+        printf "     Revoked ::   %s\n" "$NAME"
+    else
+        printf "     Unknown ::   %s\n" "$NAME"
+    fi
+done <${INDEX}
+printf "\n"
+
+
+
+
+
+}
 function manageMenu () {
 	clear
 	echo "Welcome to OpenVPN-install!"
