62a4ff3b41d562c1d5c82dd48ff34aa36780b160
randomshell <43271778+randomshell@users.noreply.github.com>
2020-04-27 12:19:25 +0000

    fix(client conf): ignore block-outside-dns if not supported (#628)

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -1020,6 +1020,7 @@ cipher $CIPHER
 tls-client
 tls-version-min 1.2
 tls-cipher $CC_CIPHER
+ignore-unknown-option block-outside-dns
 setenv opt block-outside-dns # Prevent Windows 10 DNS leak
 verb 3" >> /etc/openvpn/client-template.txt
 
