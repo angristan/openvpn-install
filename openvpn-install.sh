9e1fe367bf8452fa19d2d5b46cb8263792f4f9d7
robiiinos <dyrda.robin@outlook.fr>
2020-06-29 09:09:38 +0200

    Default DNS to AdGuard

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -315,7 +315,7 @@ function installQuestions() {
 	echo "   12) NextDNS (Anycast: worldwide)"
 	echo "   13) Custom"
 	until [[ $DNS =~ ^[0-9]+$ ]] && [ "$DNS" -ge 1 ] && [ "$DNS" -le 13 ]; do
-		read -rp "DNS [1-12]: " -e -i 3 DNS
+		read -rp "DNS [1-12]: " -e -i 11 DNS
 		if [[ $DNS == 2 ]] && [[ -e /etc/unbound/unbound.conf ]]; then
 			echo ""
 			echo "Unbound is already installed."
