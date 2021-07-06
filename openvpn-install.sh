bcd1d8a53ec7b5a5d7f96ca10be0d4055b1b3596
Stanislas <stanislas.lange@pm.me>
2021-07-06 10:56:15 +0200

    Document Rocky Linux support

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # shellcheck disable=SC1091,SC2164,SC2034,SC1072,SC1073,SC1009
 
-# Secure OpenVPN server installer for Debian, Ubuntu, CentOS, Amazon Linux 2, Fedora, Oracle Linux 8 and Arch Linux
+# Secure OpenVPN server installer for Debian, Ubuntu, CentOS, Amazon Linux 2, Fedora, Oracle Linux 8, Arch Linux and Rocky Linux.
 # https://github.com/angristan/openvpn-install
 
 function isRoot() {
