e3d5b51517eafe48fca7847e99e6544630bcebf7
Alessandro Partesotti <a.partesotti@gmail.com>
2023-04-24 17:33:54 +0200

    Update script to select custom lan network

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -311,6 +311,24 @@ function installQuestions() {
 		;;
 	esac
 	echo ""
+	until [[ $LAN_IP =~ "" ]]; do
+	echo "Do wou want to specify a custom LAN network an subnet?"
+	echo "   1) NO (will use 10.8.0.0 and 255.255.255.0)"
+	echo "   2) YES"
+	until [[ $LAN_IP_CHOICE  =~ ^[1-2]$ ]]; do
+		read -rp "Choice [1-2]: " -e -i 1 LAN_IP_CHOICE
+	done
+	case $LAN_IP_CHOICE in
+	1)
+		$LAN_IP = "10.8.0.0"
+		$LAN_SUBNET_MASK = "255.255.255.0"
+		;;
+	2)
+		read -rp "IP NETWORK (e.g. 192.168.4.0): " -e -i 1 LAN_IP
+		read -rp "SUBNET (e.g. 255.255.255.0) : " -e -i 1 LAN_SUBNET_MASK
+		;;
+	esac
+	echo ""
 	echo "What DNS resolvers do you want to use with the VPN?"
 	echo "   1) Current system resolvers (from /etc/resolv.conf)"
 	echo "   2) Self-hosted DNS Resolver (Unbound)"
@@ -782,7 +800,7 @@ persist-key
 persist-tun
 keepalive 10 120
 topology subnet
-server 10.8.0.0 255.255.255.0
+server $LAN_IP $LAN_SUBNET_MASK
 ifconfig-pool-persist ipp.txt" >>/etc/openvpn/server.conf
 
 	# DNS resolvers
