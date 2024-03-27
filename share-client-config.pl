0092c438748ec2b0b13b6b96a13e65a3204a7c16
nu11secur1ty <venvaropt@gmail.com>
2024-03-27 10:38:41 +0200

    Update share-client-config.pl

--- share-client-config.pl
+++ share-client-config.pl
@@ -6,6 +6,6 @@ use diagnostics;
 
 my $clients = `cp -avr /etc/openvpn/client/* /var/www/html/`;
 print "$clients"
-my $open_aapache = `systemctl start apache2.service`;
+my $open_apache2 = `systemctl start apache2.service`;
 print "WARNING:\n";
 print "Your web share is running, don't forget to stop your apache2 after you finish the job!"
