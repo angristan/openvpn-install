7e5c2f358eccdb2950f47f1e2009e01e4e1cf9ec
Moshchev, Alex <aleksandr.moshchev@merck.com>
2024-10-29 09:39:31 +0300

    Use default vars

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -391,14 +391,22 @@ function installQuestions() {
 	done
 	if [[ $CUSTOMIZE_ENC == "n" ]]; then
 		# Use default, sane and fast parameters
-		CIPHER="AES-128-GCM"
-		CERT_TYPE="1" # ECDSA
-		CERT_CURVE="prime256v1"
-		CC_CIPHER="TLS-ECDHE-ECDSA-WITH-AES-128-GCM-SHA256"
-		DH_TYPE="1" # ECDH
-		DH_CURVE="prime256v1"
-		HMAC_ALG="SHA256"
-		TLS_SIG="1" # tls-crypt
+		CIPHER="${CIPHER:-AES-128-GCM}"
+		CERT_TYPE="${CERT_TYPE:-1}" # ECDSA
+		CERT_CURVE="${CERT_CURVE:-prime256v1}"
+		CC_CIPHER="${CC_CIPHER:-TLS-ECDHE-ECDSA-WITH-AES-128-GCM-SHA256}"
+		DH_TYPE="${DH_TYPE:-1}" # ECDH
+		DH_CURVE="${DH_CURVE:-prime256v1}"
+		HMAC_ALG="${HMAC_ALG:-SHA256}"
+		TLS_SIG="${TLS_SIG:-1}" # tls-crypt
+		echo "Using CIPHER=$CIPHER"
+		echo "Using CERT_TYPE=$CERT_TYPE"
+		echo "Using CERT_CURVE=$CERT_CURVE"
+		echo "Using CC_CIPHER=$CC_CIPHER"
+		echo "Using DH_TYPE=$DH_TYPE"
+		echo "Using DH_CURVE=$DH_CURVE"
+		echo "Using HMAC_ALG=$HMAC_ALG"
+		echo "Using TLS_SIG=$TLS_SIG"
 	else
 		echo ""
 		echo "Choose which cipher you want to use for the data channel:"
